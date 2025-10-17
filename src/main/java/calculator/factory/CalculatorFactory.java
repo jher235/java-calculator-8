da769b2b4fc08158279c293b0fe54b2aa668b7de
jher235 <tim668666@gmail.com>
2025-10-18 01:08:37 +0900

    refactor: 의존성 주입 시 전략 패턴을 사용하도로 변경

--- src/main/java/calculator/factory/CalculatorFactory.java
+++ src/main/java/calculator/factory/CalculatorFactory.java
@@ -5,15 +5,32 @@ import calculator.io.IOConsoleHandler;
 import calculator.io.IOHandler;
 import calculator.parser.IntegerParser;
 import calculator.parser.Parser;
+import java.util.Map;
 
 public class CalculatorFactory {
 
-    public Calculator<Integer> createCalculator() {
+    private static final Map<Class<?>, Parser<?>> parserHandler = Map.of(
+        Integer.class, new IntegerParser()
+    );
+
+    public <T> Calculator<T> createCalculator(Class<T> type) {
         IOHandler iOHandler =  new IOConsoleHandler();
-        Parser<Integer> parser  = new IntegerParser();
+        Parser<T> parser = getParser(type);
         return new Calculator<>(
             iOHandler,
             parser
         );
     }
+
+    @SuppressWarnings("unchecked")
+    private <T> Parser<T> getParser(Class<T> type){
+        Parser<T> parser = (Parser<T>) parserHandler.get(type);
+
+        if(parser == null){
+            throw new IllegalArgumentException("Not Found Parser: " + type.getName());
+        }
+
+        return parser;
+    }
+
 }
