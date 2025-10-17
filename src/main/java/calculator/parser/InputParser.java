8102362b7532270f812fe437cb1de7cec9914da2
jher235 <tim668666@gmail.com>
2025-10-17 21:25:41 +0900

    feat: Input 객체로부터 Integer 목록을 추출하는 로직 구현

---
+++ src/main/java/calculator/parser/InputParser.java
@@ -0,0 +1,58 @@
+package calculator.parser;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputParser {
+
+    private static final String CUSTOM_DELIMITER_RANGE_PARSER = "(?s)^(//.*\\\\n)(.*)$";
+    private static final String CUSTOM_DELIMITER_PARSER = "//(.*?)\\\\n";
+
+    private static final List<String> defaultDelimiters = List.of( ",", ";");
+
+    public List<Integer> parseToIntegers(Input<String> input){
+        String inputValue = input.getValue();
+
+        List<String> delimiters = new ArrayList<>(defaultDelimiters);
+        String inputWithoutDelimiter = parseDelimiter(inputValue, delimiters);
+
+        return Arrays.stream(inputWithoutDelimiter.split(getDelimiterRegex(delimiters)))
+            .map(Integer::parseInt)
+            .toList();
+    }
+
+    private String getDelimiterRegex(List<String> delimiters) {
+        return String.join("|", delimiters);
+    }
+
+    private String parseDelimiter(String input, List<String> delimiters){
+        if(input != null && input.startsWith("//")){
+            Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_RANGE_PARSER);
+            Matcher matcher = pattern.matcher(input);
+
+            if(!matcher.matches())
+                throw new IllegalArgumentException("invalid custom delimiter request");
+
+            String delimiterInput = matcher.group(1);
+            String inputWithoutDelimiter = matcher.group(2);
+
+            parseDelimiters(delimiterInput, delimiters);
+            return inputWithoutDelimiter;
+        }
+        return input;
+    }
+
+    private List<String> parseDelimiters(String input, List<String> delimiters){
+        Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_PARSER);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            delimiters.add(matcher.group(1));
+        }
+
+        return delimiters;
+    }
+}
