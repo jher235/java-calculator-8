7e99515eb3f8c736bc50af982173cafba412138d
jher235 <tim668666@gmail.com>
2025-10-20 10:50:14 +0900

    chore: 주석 추가

--- src/main/java/calculator/io/IOHandler.java
+++ src/main/java/calculator/io/IOHandler.java
@@ -1,5 +1,8 @@
 package calculator.io;
 
+/**
+ * 인터페이스 네이밍이 IOHandler 지만... calculator.io.IOHandler 이므로 Calculate에 의존적인 메서드를 포함하도록 함
+ */
 public interface IOHandler {
 
     Input<String> requestStringInput();
